f2a4ea8a155e1644743b6f4d1f9b43a2c4f0e56a
Jerry <jerrylabs@seznam.cz>
2019-09-21 13:43:05 +0200

    joints and attributes

--- cmzc/card.js
+++ cmzc/card.js
@@ -1,8 +1,59 @@
+shortcut = (c) => {
+  switch (c) {
+    case 'B': return 'bone';
+    case 'H': return 'hand';
+    case 'L': return 'leg';
+    case 'P' : return 'power';
+    case 'S' : return 'speed';
+    case 'I' : return 'iq';
+    case 'Z' : return 'heart';
+    default: return undefined;
+  }
+}
+
+getJointMarkup = (order, rel, type) =>
+  `<img class="joint ${rel} ${order} ${type}" src="http://localhost:8080/${rel}-${type}.png" />`;
+
+getAttrMarkup = (c, i) =>
+`<img class="attribute" src="http://localhost:8080/${c == 'L' ? '' : 'attr-'}${shortcut(c)}.png" />`;
+
+getFieldMarkup = (title, value, card) => {
+  let markup;
+  switch (title) {
+
+    /* nazev karty */
+    case 'title':
+      markup = `<div class="${title}"><span>${value}</span></div>`;
+      if (card.text == '') {
+        markup += `<div class="title title-reversed"><span>${value}</span></div>`;
+      }
+    break;
+
+    /* vyzadovane a poskytovane kosti, ruce a nohy */
+    case 'requires':
+    case 'provides':
+      const relation = title.replace('s', 'd');
+      if (value.length == 1) {
+        markup = getJointMarkup('single', relation, shortcut(value));
+      } else if (value.length == 2) {
+        markup = `${getJointMarkup('first', relation, shortcut(value[0]))}
+          ${getJointMarkup('second', relation, shortcut(value[1]))}`;
+      }
+    break;
+    case 'attributes':
+      markup = value.split('').map(getAttrMarkup).join('');
+    break;
+    default:
+      markup = `<div class="${title}">${value}</div>`;
+  }
+  return markup;
+}
+
 module.exports = (cardData) => {
   return `<div class="card ${cardData.typ || ''}"> ${
     Object.keys(cardData).map(cardProperty =>
       cardData[cardProperty]
-      ? `<div class="${cardProperty}">${cardData[cardProperty]}</div>` // here condition for specific fields
+      ? getFieldMarkup(cardProperty, cardData[cardProperty], cardData)
       : ''
     ).join('')
   }</div>`;
