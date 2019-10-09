95948b2a629abddf15150fe531c96bddb8bee7b6
Jerry <jerrylabs@seznam.cz>
2019-10-09 23:37:24 +0200

    tests for tech cards (except smileys)

--- cmzc/card.js
+++ cmzc/card.js
@@ -18,6 +18,29 @@ const getLiveMarkup = (c, i) => `<img class="life" src="http://localhost:8080/${
 
 const getAttrMarkup = (c, i) => `<img class="attribute attribute-${shortcut(c)}" src="http://localhost:8080/attr-${shortcut(c)}.png" />`;
 
+const getRerollsMarkup = (value) => {
+  let markup = '';
+
+  /* pozadavek na test, je-li nejaky */
+  if (value.includes('P') || value.includes('S') || value.includes('I')){
+    let testImgNamePart;
+    if (value.includes('PI'))      { testImgNamePart = 'poweriq'; }
+    else if (value.includes('PS')) { testImgNamePart = 'powerspeed'; }
+    else if (value.includes('P'))  { testImgNamePart = 'power'; }
+    else if (value.includes('S'))  { testImgNamePart = 'speed'; }
+    else if (value.includes('I'))  { testImgNamePart = 'iq'; }
+    markup = `<img class="test" src="http://localhost:8080/test-${testImgNamePart}.png" />`;
+  }
+  let diceAmount;
+  if (diceAmount = value.match(/[1-3]/g)) {
+    markup = `${markup}
+      <div class="reroll reroll${value.includes('X') ? '-red' : '-green'}">
+        <img src="http://localhost:8080/dice${diceAmount}.png" />
+      </div>`;
+  }
+  return markup;
+}
+
 const getFieldMarkup = (title, value, card) => {
   let markup;
   switch (title) {
@@ -54,6 +77,9 @@ const getFieldMarkup = (title, value, card) => {
     case 'image':
       markup = `<img src="http://localhost:8080/imgs/${value}.png" class="illustration" />`;
     break;
+    case 'rerolls':
+      markup = `<div class="rerolls">${getRerollsMarkup(value)}</div>`;
+    break;
     default:
       markup = `<div class="${title}">${value}</div>`;
   }
@@ -61,7 +87,7 @@ const getFieldMarkup = (title, value, card) => {
 }
 
 const isHorizontal = (cardData) =>
-  cardData.type != 'voodoo' && !cardData.requires && !cardData.provides && !cardData.text;
+  cardData.type != 'voodoo' && !cardData.requires && !cardData.provides && !cardData.text && !cardData.rerolls;
 
 const isVertical = (cardData) =>
   cardData.type != 'voodoo' && cardData.text
