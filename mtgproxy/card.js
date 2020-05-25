c40e82d08ced505de5086324fa6f2f80d2fafa65
Jerry <jerrylabs@seznam.cz>
2020-05-25 18:53:40 +0200

    ?

--- mtgproxy/card.js
+++ mtgproxy/card.js
@@ -1,6 +1,6 @@
 module.exports = (cardData) => {
   return `<div
     class="card"
-    style="background-image: url(https://img.scryfall.com/cards/large/front${cardData.image});">
+    style="background-image: url(${cardData.image});">
     </div>`;
 }
