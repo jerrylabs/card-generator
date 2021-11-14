9bdc97fd62de09fb02c2bbd5618c6a990dbb97ab
Jerry <jerrylabs@seznam.cz>
2021-11-14 23:13:29 +0100

    Car type added

--- kta/card.js
+++ kta/card.js
@@ -12,14 +12,16 @@ module.exports = (cardData) => {
       <div class="frame"></div>
       <div class="title${titleLong}"><span>${cardData.title}</span></div>
       <div class="title${titleLong} shadow"><span>${cardData.title}</span></div>
-        <img class="logo" src="http://localhost:8080/logos/${cardData.logo}.png" />
-        <div class="ilus" style="background-image: url('http://localhost:8080/ilus/${cardData.pic}');" /></div>
-        <div class="stats">
-          <div class="stat acc">${cardData.acc}</div>
-          <div class="stat grip">${cardData.endurance}</div>
-          <div class="stat grip shadow">${cardData.endurance}</div>
-          <div class="stat max">${cardData.speed}</div>
-        </div>
+      <div class="level"><span>${cardData.level}</span></div>
+      <div class="level shadow"><span>${cardData.level}</span></div>
+      <img class="logo" src="http://localhost:8080/logos/${cardData.logo}.png" />
+      <div class="ilus" style="background-image: url('http://localhost:8080/ilus/${cardData.pic}');" /></div>
+      <div class="stats">
+        <div class="stat acc">${cardData.acc}</div>
+        <div class="stat grip">${cardData.endurance}</div>
+        <div class="stat grip shadow">${cardData.endurance}</div>
+        <div class="stat max">${cardData.speed}</div>
+      </div>
       ` : ``
     }
     ${cardData.type == 'mission'
