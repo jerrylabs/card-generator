32f0a79b6852b84b755b88cd9ab045789032af65
Jerry <jerrylabs@seznam.cz>
2022-04-06 19:59:22 +0200

    Gwent start

--- gwent/card.js
+++ gwent/card.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
+
 module.exports = (cardData) => {
 
-  const {title, fraction, flavor} = cardData;
+  const {title, power, faction, type, effect, flavor} = cardData;
+
+  const imageType = fs.existsSync(`./${process.argv[2]}/images/${title}.jpg`) ? 'jpg' : 'webp';
 
   return (
-    `<div class="card ${fraction}" style="background-image: url(images/${title}.jpg);">
+    `<div class="card ${faction}" style="background-image: url('http://localhost:8080/images/${title}.${imageType}');">
       <div class="title">${title}</div>
       <hr>
       <div class="flavor">${flavor}</div>
