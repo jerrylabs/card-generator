992cf762f4a8962fbcbb3136c5d93df11e6deaeb
Jaroslav Kameň <jaroslav.kamen@firma.seznam.cz>
2025-08-24 21:41:28 +0200

    Mystic elements white images

--- mysticelements/card.js
+++ mysticelements/card.js
@@ -1,6 +1,15 @@
 module.exports = (cardData) => {
-  return `<div
-    class="card"
-    style="background-image: url(${cardData.image});">
+  return `<div class="card">
+      <div class="card__edge"></div>
+      <div class="card__frame card__frame--${cardData.color}"></div>
+      <div class="card__name">${cardData.name}</div>
+      <div class="card__cost">${cardData.cost}</div>
+      <div class="card__image">
+        <img src="ilus/${cardData.name}.png" alt="${cardData.name}">
+      </div>
+      <div class="card__status">${cardData.status}</div>
+      <div class="card__text">${cardData.text}</div>
+      ${cardData.power ? `<div class="card__power">${cardData.power}</div>` : ''}
+      ${cardData.health ? `<div class="card__health">${cardData.health}</div>` : ''}
     </div>`;
 }
